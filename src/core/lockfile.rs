69adddd92f1c1af83958cfe4b6a0d22dab1ecc60
root <yann-mael.bouton@epitech.eu>
2026-02-03 17:49:04 +0100

    chore(lockfile): add lockfile structure initialization

--- src/core/lockfile.rs
+++ src/core/lockfile.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Lockfile {
     pub lock_version: u32,
-    pub cache_commit: u32,
+    pub cache_commit: String,
     pub installed_modules: Vec<String>,
 
     pub modules: std::collections::HashMap<String, ModulesEntry>,
@@ -14,4 +14,15 @@ pub struct Lockfile {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ModulesEntry {
     pub files: std::collections::HashMap<String, String>,
+}
+
+impl Lockfile {
+    pub fn new() -> Self {
+        Self {
+            lock_version: 1,
+            cache_commit: String::new(),
+            installed_modules: Vec::new(),
+            modules: std::collections::HashMap::new(),
+        }
+    }
 }
