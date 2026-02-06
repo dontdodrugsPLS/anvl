39d9ffd6c35e0f670b54ae620fa2cee4c878cf1f
root <yann-mael.bouton@epitech.eu>
2026-02-06 22:09:21 +0100

    chore(paths): add find project root function

--- src/core/paths.rs
+++ src/core/paths.rs
@@ -5,3 +5,10 @@ pub struct ProjectPaths {
     pub root: PathBuf,
     pub lockfile_path: PathBuf,
 }
+
+pub fn find_project_root() -> Result<ProjectPaths, String> {
+    let mut current_path =
+        std::env::current_dir().map_err(|e| format!("failed to get current directory: {e}"))?;
+
+    Err("not inside an Anvl project (anvl.lock.json not found in parent directory)".to_string())
+}
